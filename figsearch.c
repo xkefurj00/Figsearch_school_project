51ffbe60aa3b3f27cc4f73421f956e9e5fef69ee
xkefurj00 <xkefurj00@vutbr.cz>
2024-11-18 21:47:17 +0100

    input+vline
    Added function for vline and use function

--- figsearch.c
+++ figsearch.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define FILEPATH "obrazek.txt"
 
@@ -58,11 +59,11 @@ for (int row = 0; row < image->number_of_lines; row++)
 
 }
 
-int load_file(struct Image *image){
+int load_file(struct Image *image, FILE *bitmap){
 
 
 
-fscanf(stdin,"%d %d", &image->number_of_lines, &image->number_of_columns);
+fscanf(bitmap,"%d %d", &image->number_of_lines, &image->number_of_columns);
 // Loads first two numbers which are size of image
 
 if (bitmap_ctor(image) !=0){
@@ -74,7 +75,7 @@ for (int row = 0; row < image->number_of_lines; row++){
 
     for (int column = 0; column < image->number_of_columns; column++)
     {
-        fscanf(stdin,"%d", &image->colors[row][column]);
+        fscanf(bitmap,"%d", &image->colors[row][column]);
     }
     
 }
@@ -133,28 +134,94 @@ row_number, column_number_end);
 
 }
 
-void find_vline(){
+void find_vline(struct Image *image){
     
+int longest1 = 0;
+int longest2 = 0;
+int column_number;
+int row_number_start;
+int row_number_end;
+
+
+for (int column = 0; column < image->number_of_columns; column++)
+{
+    for (int row = 0; row < image->number_of_lines; row++)
+    {
+        if (image->colors[row][column]==1)
+        {
+            longest1++;
+        }
+
+        if ((image->colors[row][column]==0)  || (row == image->number_of_lines - 1))
+        {
+                if (longest1 > longest2)
+                {
+                    longest2 = longest1;
+                    column_number = column;
+                    row_number_start = row - longest2 + 1;
+                    row_number_end = row;
+                }
+            longest1 = 0;
+        } 
+        
+        
+
+    }
+
+}
+
+fprintf(stdout,"%d %d %d %d",  row_number_start , column_number, row_number_end, column_number);
+
+
 }
 
 void find_square(){
     
 }
 
+void use_function(struct Image *image, char *function){
 
-int main(int argc, char argv[]){
+if (strcmp(function, "hline")==0)
+{
+   find_hline(image);
+}
+if (strcmp(function, "vline")==0)
+{
+   find_vline(image);
+}
+
+}
+
+
+
+int main(int argc, char *argv[]){
 
 struct Image image;
 
-FILE *bitmap=fopen(argv[1])
 
-if (load_file(&image) != 0)
+FILE *bitmap=fopen(argv[2], "r");
+if (bitmap == NULL)
 {
+    fprintf(stderr, "Error: Cant open file");
     return 1;
 }
 
-find_hline(&image);
 
+
+
+
+if (load_file(&image, bitmap) != 0)
+{
+    fclose(bitmap);
+    return 1;
+}
+
+if (argc==3)
+{
+    use_function(&image , argv[1]);    
+}
+
+fclose(bitmap);
 
 bitmap_dtor(&image);
 
