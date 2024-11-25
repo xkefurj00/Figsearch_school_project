1b39204504ed778fd17b4bf7a2b9ade65ebdf587
xkefurj00 <xkefurj00@vutbr.cz>
2024-11-25 14:46:39 +0100

    vline, hline, square
    vline, hline and square are done, need to do test and help

--- figsearch.c
+++ figsearch.c
@@ -12,6 +12,13 @@ struct Image
 };
 
 
+/*
+This fuction allocating memory for image using malloc
+First time for rows and then for columns
+It creates 2D Matrix
+Return 1 when failed
+Return 0 when succesful
+*/
 int bitmap_ctor(struct Image *image){
 
 image->colors = (int**)malloc(image->number_of_lines*sizeof(int*));
@@ -19,7 +26,7 @@ image->colors = (int**)malloc(image->number_of_lines*sizeof(int*));
 
 if (image->colors==NULL){
 
-    fprintf(stderr,"Memory allocation failed!");
+    fprintf(stderr,"Memory allocation failed! \n");
     return 1;
 
 }// Checks if malloc was successfull
@@ -30,7 +37,7 @@ for (int row = 0; row < image->number_of_lines; row++)
 
     if (image->colors[row]==NULL){
 
-    fprintf(stderr,"Memory allocation failed!");
+    fprintf(stderr,"Memory allocation failed! \n");
     return 1;
 
     }// Checks if malloc was successfull
@@ -42,7 +49,10 @@ return 0;
 }
 
 
-
+/*
+This fuction frees memory for image using free
+First time for colums and then for rows
+*/
 void bitmap_dtor(struct Image *image){
 
 for (int row = 0; row < image->number_of_lines; row++)
@@ -59,16 +69,25 @@ for (int row = 0; row < image->number_of_lines; row++)
 
 }
 
+
+
+
+
+
+
+/*
+Function for loading image from file
+Loads first 2 numbers which are sizes of matrix
+Then all numbers in image file
+*/
 int load_file(struct Image *image, FILE *bitmap){
-
-
 
 fscanf(bitmap,"%d %d", &image->number_of_lines, &image->number_of_columns);
 // Loads first two numbers which are size of image
 
 if (bitmap_ctor(image) !=0){
     return 1;
-}
+}// Calling function bitmap_ctor, return 1 if fail
 
 
 for (int row = 0; row < image->number_of_lines; row++){
@@ -93,14 +112,39 @@ void print_help(){
 
 }
 
+int test(struct Image *image, int row, int column){
+
+int is_valid = 1;
+
+if (image->number_of_columns <=0 || image->number_of_lines <= 0 )
+{
+   is_valid = 0;
+}// check if size of image is corect
+
+
+
+if (image->colors[row][column] != 1 && image->colors[row][column] != 0)
+{
+    is_valid = 0;       
+}// Check if matrix only contains 0 or 1
+
+
+if(is_valid)
+{
+   fprintf(stdout,"Invalid \n");
+}else fprintf(stdout,"Valid \n");
+
+return is_valid;
+}
+
 void find_hline(struct Image *image){
 
 int longest1 = 0;
-int longest2 = 0;
+int longest2 = -1;
 int row_number;
 int column_number_start;
 int column_number_end;
-
+int off_by_one = 1;
 
 for (int row = 0; row < image->number_of_lines; row++)
 {
@@ -111,23 +155,32 @@ for (int row = 0; row < image->number_of_lines; row++)
             longest1++;
         }
 
-        if ((image->colors[row][column]==0)  || (column == image->number_of_columns - 1))
+        if (image->colors[row][column]==0)
         {
-                if (longest1 > longest2)
-                {
-                    longest2 = longest1;
-                    row_number = row;
-                    if (column == longest2){
-                    column_number_start = column - longest2;
-                    column_number_end = column - 1;
-                    }
-                    else{
-                    column_number_start = column - longest2 +1;
-                    column_number_end = column;
-                    }
-                }
-            longest1 = 0;
-        } 
+            if (longest1 > longest2)
+            {
+                longest2 = longest1;
+                row_number = row;
+                column_number_start = column - longest2;
+                column_number_end = column - off_by_one;
+
+            }
+        longest1 = 0;
+        }
+
+        if (column == image->number_of_columns - off_by_one)
+        {
+            if (longest1 > longest2)
+            {
+                longest2 = longest1;
+                row_number = row;
+                column_number_start = column - longest2 + off_by_one;
+                column_number_end = column;
+            }
+               
+        longest1 = 0; 
+        }
+        
         
         
 
@@ -135,7 +188,7 @@ for (int row = 0; row < image->number_of_lines; row++)
 
 }
 
-fprintf(stdout,"%d %d %d %d", row_number, column_number_start ,
+fprintf(stdout,"%d %d %d %d \n", row_number, column_number_start ,
 row_number, column_number_end);
 
 }
@@ -143,41 +196,51 @@ row_number, column_number_end);
 void find_vline(struct Image *image){
     
 int longest1 = 0;
-int longest2 = 0;
+int longest2 = -1;
 int column_number;
 int row_number_start;
 int row_number_end;
+int off_by_one = 1;
 
 
 for (int column = 0; column < image->number_of_columns; column++)
 {
     for (int row = 0; row < image->number_of_lines; row++)
     {
-        if (image->colors[row][column]==1)
+        if (image->colors[row][column] == 1)
         {
             longest1++;
         }
 
-        if ((image->colors[row][column]==0)  || (row == image->number_of_lines - 1))
+        if (image->colors[row][column]==0)
         {
-                if (longest1 > longest2)
-                {
-                    longest2 = longest1;
-                    column_number = column;
-                    if (row == longest2)
-                    {
-                        row_number_start = row - longest2;
-                        row_number_end = row - 1;
-                    }else{
-                    row_number_start = row - longest2 + 1;
-                    row_number_end = row;
-                    }
-                    
-                    
-                    
-                }
-            longest1 = 0;
+            if (longest1 > longest2)
+            {
+                longest2 = longest1;
+                column_number = column;
+                row_number_start = row - longest2;
+                row_number_end = row - off_by_one;
+                  
+                   
+            }        
+        longest1 = 0;
+
         } 
+        if (row == image->number_of_lines - off_by_one)
+        {
+            if (longest1 > longest2)
+            {
+                longest2 = longest1;
+                column_number = column;
+                row_number_start = row - longest2 + off_by_one;
+                row_number_end = row;
+            }
+            
+        
+        longest1 = 0;
+        }
+        
+
         
         
 
@@ -185,7 +248,7 @@ for (int column = 0; column < image->number_of_columns; column++)
 
 }
 
-fprintf(stdout,"%d %d %d %d",  row_number_start , column_number, row_number_end, column_number);
+fprintf(stdout,"%d %d %d %d \n",  row_number_start , column_number, row_number_end, column_number);
 
 
 }
@@ -196,7 +259,7 @@ int row_number_start, row_number_end, column_number_start, column_number_end;
 int square_size = 0;
 int largest_square_size = -1;
 int counter;
-//int valid;
+
 
 for (int row = 0; row < image->number_of_lines; row++)
 {
@@ -208,7 +271,7 @@ for (int row = 0; row < image->number_of_lines; row++)
             for (square_size = 0; (square_size + column < image->number_of_columns) 
                 && (square_size + row < image->number_of_lines); square_size++)
             {  
-               //  valid = 1;
+              
 
                 if (image->colors[row + square_size][column] == 1 &&
                     image->colors[row][column + square_size] == 1)
@@ -222,7 +285,7 @@ for (int row = 0; row < image->number_of_lines; row++)
                         || (image->colors[row + counter][column + square_size] == 0))
                         {   
                         
-                      //  valid = 0;
+                      
                         break;
                     
                         }else 
@@ -237,14 +300,14 @@ for (int row = 0; row < image->number_of_lines; row++)
 
                     }
                 }else{
-               // valid = 0;
+              
                 break;
                 }
 
                
             }      
 
-          //  fprintf(stdout, "R:%d, CL:%d, S:%d, C:%d V:%d\n" , row, column, square_size, counter, valid);
+       
                 
 
         }
@@ -256,7 +319,7 @@ if (largest_square_size > -1)
 {
     fprintf(stdout, "%d %d %d %d\n", row_number_start, column_number_start,
     row_number_end, column_number_end);
-} else fprintf(stdout, "No square found.");
+} else fprintf(stdout, "No square found. \n");
 
 
 }
@@ -276,6 +339,7 @@ if (strcmp(function, "square")==0)
    find_square(image);
 }
 
+
 }
 
 
@@ -288,7 +352,7 @@ struct Image image;
 FILE *bitmap=fopen(argv[2], "r");
 if (bitmap == NULL)
 {
-    fprintf(stderr, "Error: Cant open file");
+    fprintf(stderr, "Error: Cant open file \n");
     return 1;
 }
 
